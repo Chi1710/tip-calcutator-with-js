b5c71f5acb12705cf5621ec222687e77994bf75c
Chi <chiyantsestudio@gmail.com>
2023-01-04 22:11:34 -0500

    create a function to get input values and  to calcaute output values

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const sliders = document.querySelectorAll("input[type='range']");
-
-// console.log(sliders);
-
 sliders.forEach(function(silder){
     silder.addEventListener("input", calculateTip);
 });
 
 const billInput = document.getElementById("bill");
 billInput.addEventListener("change", calculateTip);
-console.log(billInput);
-
 
 function calculateTip() {
+    let bill = parseFloat(billInput.value);
+    let tipPercentage = document.getElementById("tip").value;
+    let numOfPeople = document.getElementById("num-of-people").value;
+  
+    // billInput.value = bill.toFixed(2);
+
+    let totalTip = parseFloat((bill * (tipPercentage/100)).toFixed(2));
+    let total = parseFloat((bill + totalTip).toFixed(2));
+
+    let tipPerPerson = ( totalTip / numOfPeople ).toFixed(2);
+    let totalPerPerson = ( total / numOfPeople ).toFixed(2);
 
 }
